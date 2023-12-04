e7b2b5a50f625f0533e35e91efbaff1acac15de7
siddharthsachdev <sidsachdev15@gmail.com>
2023-12-04 11:38:24 +0530

    initial commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,12 +5,15 @@ import React from "react";
 const LoginPage = () => {
   return (
     <div className="p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center">
+      
       {/* BOX */}
       <div className=" h-full shadow-2xl rounded-md flex flex-col md:flex-row md:h-[70%] md:w-full lg:w-[60%] 2xl:w-1/2">
+        
         {/* IMAGE CONTAINER */}
         <div className="relative h-1/3 w-full md:h-full md:w-1/2">
-          <Image src="/loginBg.png" alt="" fill className="object-cover"/>
+          <Image src="/pictures/moctails.jpg" alt="" fill className="object-cover"/>
         </div>
+        
         {/* FORM CONTAINER */}
         <div className="p-10 flex flex-col gap-8 md:w-1/2">
           <h1 className="font-bold text-xl xl:text-3xl">Welcome</h1>
